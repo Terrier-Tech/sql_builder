a1e5ff366b15c2c4a7647eb8b7bb2e1e14e356d7
Andy Selvig <ajselvig@gmail.com>
2020-04-11 11:51:56 -0500

    initial repo

---
+++ lib/sql/builder.rb
@@ -0,0 +1,8 @@
+require "sql/builder/version"
+
+module Sql
+  module Builder
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sql/builder/version.rb
@@ -0,0 +1,5 @@
+module Sql
+  module Builder
+    VERSION = "0.1.0"
+  end
+end
