186fb77afd50913072fdbee327464d254332ea1c
Ryan-del-Main <85709986+rdmaccess@users.noreply.github.com>
2022-11-28 13:33:22 -0600

    limit warning message fix

--- lib/sql_builder.rb
+++ lib/sql_builder.rb
@@ -276,7 +276,7 @@ class SqlBuilder
 
   def check_result_limit!(query)
     if query.count == @the_limit and @limit_warning
-      raise "Query result has exactly #{@the_limit} results, which is the same as the limit of #{@the_limit}"
+      raise "Query result has exactly #{@the_limit} results, which is the same as the limit"
     end
     query
   end
